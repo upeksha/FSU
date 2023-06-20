e6bdc982bb1c7000fbf88f1ea418c84348f9cbde
upeksha <upekshamendiz@gmail.com>
2023-06-20 13:19:49 +1200

    Update shareViaEmail.js

--- shareViaEmail.js
+++ shareViaEmail.js
@@ -2,7 +2,7 @@
 
 function shareViaEmail() {
     var subject = "Check out this webpage";
-    var body = "I found this webpage and thought you might be interested:\n\n" + window.location.href;
+    var body = "I found this webpage and thought you might be interested:\n\n" + "https://www.freespeechsubmission.com";
     var mailtoLink = "mailto:?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
     window.location.href = mailtoLink;
   }
